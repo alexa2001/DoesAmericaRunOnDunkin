22670b6c06f3a3a410885010894ec947bcf0f505
Olcott23 <olcotthailey@gmail.com>
2023-11-16 13:42:44 -0500

    started setting up structure for fast food map vis (visualization1)

--- js/fastFoodMap.js
+++ js/fastFoodMap.js
@@ -1,5 +1,51 @@
-let promises = [
-    d3.csv("data/Top 50 Fast-Food Chains in USA.csv"),
-];
+class foodMapVis {
+    constructor(parentElement, foodMapData) {
+        this.parentElement = parentElement;
+        this.mapData = foodMapData;
 
-console.log(promises);
+        // define colors
+        this.colors = ['#fddbc7', '#f4a582', '#d6604d', '#b2182b']
+
+        console.log(this.mapData)
+
+        this.initVis()
+    }
+
+    initVis() {
+        let vis = this;
+
+        // set margins, width, and height
+        vis.margin = {top: 20, right: 20, bottom: 20, left: 20};
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
+        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
+
+        // init drawing area
+        vis.svg = d3.select("#" + vis.parentElement).append("svg")
+            .attr("width", vis.width)
+            .attr("height", vis.height)
+            .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
+
+        // add title
+        vis.svg.append('g')
+            .attr('class', 'title')
+            .attr('id', 'map-title')
+            .append('text')
+            .text('Title for Map')
+            .attr('transform', `translate(${vis.width / 2}, 20)`)
+            .attr('text-anchor', 'middle');
+
+        vis.wrangleData()
+    }
+
+    wrangleData () {
+        let vis = this;
+
+        vis.updateVis()
+    }
+
+    updateVis () {
+
+        let vis = this;
+
+    }
+}
