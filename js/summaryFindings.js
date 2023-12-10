034455cae58b9cd06d37db9a9b772e8f712b2e58
Olcott23 <olcotthailey@gmail.com>
2023-12-10 15:19:19 -0500

    Adding content to summary slide

--- js/summaryFindings.js
+++ js/summaryFindings.js
@@ -26,17 +26,64 @@ class SummaryPage {
 
         vis.title = vis.svg.append("text")
             .attr("x", vis.width/2)
-            .attr("y", vis.height * 0.2)
+            .attr("y", vis.height * 0.15)
             .attr("text-anchor", "middle")
             .attr('font-size', 30)
             .attr('font-family', 'monospace')
             .attr('fill', 'black')
             .text("Takeaways")
 
-        vis.bodyText = vis.svg.append("text")
+        vis.svg.append("rect")
+            .attr("x", vis.width/2 - (vis.width/2)/2) // Subtract half of the rectangle's width
+            .attr("y", vis.height * 0.3 - 35) // y position
+            .attr("width", vis.width/2) // width
+            .attr("height", 175) // height
+            .attr("rx", 10) // horizontal corner radius
+            .attr("ry", 10) // vertical corner radius
+            .attr("stroke", "#F05E16") // border color
+            .attr("stroke-width", 2) // border width
+            .attr("fill", "white"); // fill color
+
+        // Define the lines of text
+        vis.lines = [
+            "Fast food is remains incredibly popular in the US, regardless of health data",
+            "The popularity continues as fast food is easy to obtain, cheap, and efficient",
+            "32% percent of people stated that they ate fast food because it is cheap",
+            "Americans spend over $50 Billion annually on fast food"
+        ];
+
+        // Append each line of text as a separate text element
+        vis.lines.forEach((line, i) => {
+            vis.svg.append("text")
+                .attr("x", vis.width/2)
+                .attr("y", vis.height*0.3 + i * 35)  // Increment the y attribute for each line
+                .attr("text-anchor", "middle")     // Center the text
+                .attr("font-size", 20)             // Font size
+                .attr('font-family', 'Avenir, serif')
+                .text(line);
+        });
+
+        vis.svg.append("text")
             .attr("x", vis.width/2)
-            .attr("y", vis.height * 0.4)
+            .attr("y", vis.height * 0.6)
             .attr("text-anchor", "middle")
+            .attr('font-size', 20)
+            .attr('font-family', 'monospace')
+            .attr('fill', 'black')
+            .append("tspan")
+            .text("Despite common perception, there is not much of a correlation")
+            .append("tspan")
+            .attr("x", vis.width/2)
+            .attr("dy", "2em") // Adjust this value as needed
+            .text("between median household income and the number of fast food restaurants.")
+            .append("tspan")
+            .attr("x", vis.width/2)
+            .attr("dy", "2em") // Adjust this value as needed
+            .text("Instead, the number of fast food restaurants in a given state seems")
+            .append("tspan")
+            .attr("x", vis.width/2)
+            .attr("dy", "2em") // Adjust this value as needed
+            .text("more related to the size of that state than median income")
     }
 }
 
